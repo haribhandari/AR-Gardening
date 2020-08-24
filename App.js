a3f89e32379deaf1a86e3ffd7a4fcdf04bab6e30
pranay <pranay.khurana99@gmail.com>
2020-08-24 11:56:57 +1000

    Setup Databse connection

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase/app';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSGxUwqhBcKfLIfgC51-aiuv1B-XoQkX8",
+  authDomain: "ar-gardening-5eeb1.firebaseapp.com",
+  databaseURL: "https://ar-gardening-5eeb1.firebaseio.com",
+  projectId: "ar-gardening-5eeb1",
+  storageBucket: "ar-gardening-5eeb1.appspot.com",
+  messagingSenderId: "461196396366",
+  appId: "1:461196396366:web:7b5b3f8ea9c5ad8303801f"
+};
+
+function connectToDatabase(){
+  let connection = firebase.initializeApp(firebaseConfig);
+  console.log(connection);
+}
+
+// const reference = firebase.database().ref('/test').once('value').then(snapshot => {
+//   console.log('User data: ', snapshot.val());
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  connectToDatabase();
+    return (
+      <View style={styles.container} >
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
