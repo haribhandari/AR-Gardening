251d377c86d5e586ae68b977e6095d628b6577d4
Wenze Li <wenze1996@gmail.com>
2020-09-10 00:58:49 +1000

    add 3D object

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -2,12 +2,20 @@
 
 import React, { Component } from 'react';
 
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import {
+  ViroBox,
   ViroARScene,
   ViroText,
   ViroConstants,
+  ViroMaterials,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroSpotLight,
+  ViroARPlaneSelector,
+  ViroNode,
+  ViroAnimations
 } from 'react-viro';
 
 export default class HelloWorldSceneAR extends Component {
@@ -17,7 +25,7 @@ export default class HelloWorldSceneAR extends Component {
 
     // Set initial state here
     this.state = {
-      text : "Initializing AR..."
+      text: "Initializing AR..."
     };
 
     // bind 'this' to functions
@@ -28,6 +36,38 @@ export default class HelloWorldSceneAR extends Component {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
         <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+        {/* <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} materials={["grid"]} animation={{name: "rotate", run: true, loop: true}}/> */}
+        <ViroAmbientLight color={"#FFFFFF"} />
+        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0, -1, -.2]}
+          position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
+        <ViroNode position={[0, -1, 0]} dragType="FixedToWorld" onDrag={() => { }} >
+          <Viro3DObject
+            source={require('./res/emoji_smile/emoji_smile.vrx')}
+            resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
+            require('./res/emoji_smile/emoji_smile_normal.png'),
+            require('./res/emoji_smile/emoji_smile_specular.png')]}
+            position={[0, .5, 0]}
+            scale={[.2, .2, .2]}
+            type="VRX" />
+
+          <Viro3DObject
+            source={require('./res/plant/01Alocasia_obj.obj')}
+            resources={[
+              require('./res/plant/01Alocasia_obj.mtl'),
+              require('./res/plant/gorshok.jpg'),
+              require('./res/plant/leaf.jpg'),
+              require('./res/plant/leaf_normal.jpg'),
+              require('./res/plant/moh.jpg'),
+              require('./res/plant/sand.jpg'),
+              require('./res/plant/sand_normal.jpg'),
+              require('./res/plant/stem.jpg'),
+            ]}
+            position={[0, -10, -10]}
+            scale={[.005, .005, .001]}
+            type="OBJ"
+
+          />
+        </ViroNode>
       </ViroARScene>
     );
   }
@@ -35,7 +75,7 @@ export default class HelloWorldSceneAR extends Component {
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "Hello World!"
+        text: ""
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
@@ -49,8 +89,22 @@ var styles = StyleSheet.create({
     fontSize: 30,
     color: '#ffffff',
     textAlignVertical: 'center',
-    textAlign: 'center',  
+    textAlign: 'center',
   },
 });
+
+// ViroMaterials.createMaterials({
+//   grid: {
+//     diffuseTexture: require('./res/grid_bg.jpg'),
+//   },
+// });
+// ViroAnimations.registerAnimations({
+//   rotate: {
+//     properties: {
+//       rotateY: "+=90"
+//     },
+//     duration: 250, //.25 seconds
+//   },
+// });
 
 module.exports = HelloWorldSceneAR;
