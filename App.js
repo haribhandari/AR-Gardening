2edc326bd6bd8374415389ee3ac86ba6e6ac7d09
Wenze Li <wenze1996@gmail.com>
2020-09-28 04:49:57 +1000

    Object list

--- App.js
+++ App.js
@@ -1,187 +1,143 @@
-/**
- * Copyright (c) 2017-present, Viro, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  ActivityIndicator,
   Text,
   View,
   StyleSheet,
-  PixelRatio,
   TouchableHighlight,
+  Image,
+  Alert,
 } from 'react-native';
 
 import {
-  ViroVRSceneNavigator,
   ViroARSceneNavigator
 } from 'react-viro';
 
-/*
- TODO: Insert your API key below
- */
-var sharedProps = {
-  apiKey:"API_KEY_HERE",
-}
+import renderIf from './js/helpers/renderIf';
+var InitialARScene = require('./js/ARGardening');
 
-// Sets the default scene you want for AR and VR
-var InitialARScene = require('./js/HelloWorldSceneAR');
-var InitialVRScene = require('./js/HelloWorldScene');
-
-var UNSET = "UNSET";
-var VR_NAVIGATOR_TYPE = "VR";
-var AR_NAVIGATOR_TYPE = "AR";
-
-// This determines which type of experience to launch in, or UNSET, if the user should
-// be presented with a choice of AR or VR. By default, we offer the user a choice.
-var defaultNavigatorType = UNSET;
+// Array of 3d models that we use in this sample. This app switches between this these models.
+var objArray = [
+  require('./js/res/plant2/indoorplant_02.obj'),
+  require('./js/res/plant3/12974_crocus_flower_v1_l3.obj'),
+  require('./js/res/plant4/tree.obj')];
 
 export default class ViroSample extends Component {
   constructor() {
     super();
 
+    this._onShowObject = this._onShowObject.bind(this);
+    this._renderTrackingText = this._renderTrackingText.bind(this);
+    this._onTrackingInit = this._onTrackingInit.bind(this);
+    this._onDisplayDialog = this._onDisplayDialog.bind(this);
+    this._onLoadStart = this._onLoadStart.bind(this);
+    this._onLoadEnd = this._onLoadEnd.bind(this);
+
     this.state = {
-      navigatorType : defaultNavigatorType,
-      sharedProps : sharedProps
+      viroAppProps: {displayObject:false, objectSource:objArray[0], yOffset:0, _onLoadEnd: this._onLoadEnd, _onLoadStart: this._onLoadStart, _onTrackingInit:this._onTrackingInit},
+      trackingInitialized: false,
+      isLoading: false,
     }
-    this._getExperienceSelector = this._getExperienceSelector.bind(this);
-    this._getARNavigator = this._getARNavigator.bind(this);
-    this._getVRNavigator = this._getVRNavigator.bind(this);
-    this._getExperienceButtonOnPress = this._getExperienceButtonOnPress.bind(this);
-    this._exitViro = this._exitViro.bind(this);
   }
 
-  // Replace this function with the contents of _getVRNavigator() or _getARNavigator()
-  // if you are building a specific type of experience.
   render() {
-    if (this.state.navigatorType == UNSET) {
-      return this._getExperienceSelector();
-    } else if (this.state.navigatorType == VR_NAVIGATOR_TYPE) {
-      return this._getVRNavigator();
-    } else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
-      return this._getARNavigator();
-    }
-  }
-
-  // Presents the user with a choice of an AR or VR experience
-  _getExperienceSelector() {
     return (
       <View style={localStyles.outer} >
-        <View style={localStyles.inner} >
+        <ViroARSceneNavigator style={localStyles.arView} apiKey="YOUR API KEY"
+          initialScene={{scene:InitialARScene, passProps:{displayObject:this.state.displayObject}}}  viroAppProps={this.state.viroAppProps}
+        />
 
-          <Text style={localStyles.titleText}>
-            Choose your desired experience:
-          </Text>
+        {this._renderTrackingText()}
 
+        {renderIf(this.state.isLoading,
+          <View style={{position:'absolute', left:0, right:0, top:0, bottom:0, alignItems: 'center', justifyContent:'center'}}>
+            <ActivityIndicator size='large' animating={this.state.isLoading} color='#ffffff'/>
+          </View>)
+        }
+
+        <View style={{position: 'absolute',  left: 0, right: 0, bottom: 77, alignItems: 'center'}}>
           <TouchableHighlight style={localStyles.buttons}
-            onPress={this._getExperienceButtonOnPress(AR_NAVIGATOR_TYPE)}
-            underlayColor={'#68a0ff'} >
-
-            <Text style={localStyles.buttonText}>AR</Text>
-          </TouchableHighlight>
-
-          <TouchableHighlight style={localStyles.buttons}
-            onPress={this._getExperienceButtonOnPress(VR_NAVIGATOR_TYPE)}
-            underlayColor={'#68a0ff'} >
-
-            <Text style={localStyles.buttonText}>VR</Text>
+            onPress={this._onDisplayDialog}
+            underlayColor={'#00000000'} >
+            <Image source={require("./js/res/btn_mode_objects.png")} />
           </TouchableHighlight>
         </View>
       </View>
     );
   }
 
-  // Returns the ViroARSceneNavigator which will start the AR experience
-  _getARNavigator() {
-    return (
-      <ViroARSceneNavigator {...this.state.sharedProps}
-        initialScene={{scene: InitialARScene}} />
-    );
-  }
-  
-  // Returns the ViroSceneNavigator which will start the VR experience
-  _getVRNavigator() {
-    return (
-      <ViroVRSceneNavigator {...this.state.sharedProps}
-        initialScene={{scene: InitialVRScene}} onExitViro={this._exitViro}/>
-    );
+  // Invoked when a model has started to load, we show a loading indictator.
+  _onLoadStart() {
+    this.setState({
+      isLoading: true,
+    });
   }
 
-  // This function returns an anonymous/lambda function to be used
-  // by the experience selector buttons
-  _getExperienceButtonOnPress(navigatorType) {
-    return () => {
-      this.setState({
-        navigatorType : navigatorType
-      })
+  // Invoked when a model has loaded, we hide the loading indictator.
+  _onLoadEnd() {
+    this.setState({
+      isLoading: false,
+    });
+  }
+
+  _renderTrackingText() {
+    if(this.state.trackingInitialized) {
+      return (<View style={{position: 'absolute', backgroundColor:"#ffffff22", left: 30, right: 30, top: 30, alignItems: 'center'}}>
+        <Text style={{fontSize:12, color:"#ffffff"}}>Tracking initialized.</Text>
+      </View>);
+    } else {
+      return (<View style={{position: 'absolute', backgroundColor:"#ffffff22", left: 30, right: 30, top:30, alignItems: 'center'}}>
+        <Text style={{fontSize:12, color:"#ffffff"}}>Waiting for tracking to initialize.</Text>
+        </View>);
     }
   }
 
-  // This function "exits" Viro by setting the navigatorType to UNSET.
-  _exitViro() {
+  _onTrackingInit() {
     this.setState({
-      navigatorType : UNSET
-    })
+      trackingInitialized: true,
+    });
+  }
+
+  _onDisplayDialog() {
+    Alert.alert(
+    'Choose an object',
+    'Select an object to place in the world!',
+    [
+      {text: 'plan1', onPress: () => this._onShowObject(0, "coffee_mug", 0)},
+      {text: 'plan2', onPress: () => this._onShowObject(1, "flowers", .290760)},
+      {text: 'plan3', onPress: () => this._onShowObject(2, "smile_emoji", .497823)},
+    ],
+    );
+  }
+
+  _onShowObject(objIndex, objUniqueName, yOffset) {
+    this.setState({
+        viroAppProps:{...this.state.viroAppProps, displayObject: true, yOffset: yOffset, displayObjectName: objUniqueName, objectSource:objArray[objIndex]},
+    });
   }
 }
 
 var localStyles = StyleSheet.create({
-  viroContainer :{
-    flex : 1,
-    backgroundColor: "black",
-  },
   outer : {
     flex : 1,
-    flexDirection: 'row',
-    alignItems:'center',
-    backgroundColor: "black",
   },
-  inner: {
-    flex : 1,
-    flexDirection: 'column',
-    alignItems:'center',
-    backgroundColor: "black",
+
+  arView: {
+    flex:1,
   },
-  titleText: {
-    paddingTop: 30,
-    paddingBottom: 20,
-    color:'#fff',
-    textAlign:'center',
-    fontSize : 25
-  },
-  buttonText: {
-    color:'#fff',
-    textAlign:'center',
-    fontSize : 20
-  },
+
   buttons : {
     height: 80,
-    width: 150,
+    width: 80,
     paddingTop:20,
     paddingBottom:20,
     marginTop: 10,
     marginBottom: 10,
-    backgroundColor:'#68a0cf',
+    backgroundColor:'#00000000',
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: '#fff',
-  },
-  exitButton : {
-    height: 50,
-    width: 100,
-    paddingTop:10,
-    paddingBottom:10,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor:'#68a0cf',
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#fff',
+    borderColor: '#ffffff00',
   }
 });
 
