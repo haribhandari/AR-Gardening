21a135a0b7b9bc84d5eaaaf0dca00cdd301c4ea4
Hari Bhandari <hari@192-168-1-115.tpgi.com.au>
2020-09-28 12:49:53 +1000

    slide show

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -89,7 +89,38 @@ export default class App extends React.Component {
           numColumns={2}
         />
         <Text style={styles.text}>Plant Joke of the day!</Text>
-        <SliderBox images={this.state.images} />
+        <SliderBox
+          //ImageComponent={FastImage}
+          images={this.state.images}
+          sliderBoxHeight={200}
+          dotColor="#000000"
+          inactiveDotColor="#90A4AE"
+          paginationBoxVerticalPadding={20}
+          autoplay
+          circleLoop
+          resizeMethod="resize"
+          resizeMode="cover"
+          paginationBoxStyle={{
+            position: 'absolute',
+            bottom: 0,
+            padding: 0,
+            alignItems: 'center',
+            alignSelf: 'center',
+            justifyContent: 'center',
+            paddingVertical: 20
+          }}
+          dotStyle={{
+            width: 10,
+            height: 10,
+            borderRadius: 5,
+            marginHorizontal: 0,
+            padding: 0,
+            margin: 0,
+            backgroundColor: 'rgba(128, 128, 128, 0.92)'
+          }}
+          ImageComponentStyle={{ borderRadius: 15, width: '97%', marginTop: 5 }}
+          imageLoadingColor="#2196F3"
+        />
       </View>
     );
   }
